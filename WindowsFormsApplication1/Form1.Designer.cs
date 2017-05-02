4af8155c3b6a99a262ea85da9405c7b33d46a3b8
WillPerfect <xingkong2000cn@gmail.com>
2017-05-02 13:54:11 +0800

    添加工程

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(532, 409);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(524, 383);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(518, 377);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(524, 383);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "URL：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "推广单元：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(103, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(-134, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(650, 108);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "获取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button4);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.listBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(524, 383);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "群发";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "获取QQ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(32, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 124);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.PasswordTextBox);
+            this.tabPage4.Controls.Add(this.UserNameTextBox);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Controls.Add(this.label3);
+            this.tabPage4.Controls.Add(this.button5);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(524, 383);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(124, 112);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(153, 21);
+            this.PasswordTextBox.TabIndex = 4;
+            this.PasswordTextBox.Text = "jfKSK941";
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(124, 73);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(153, 21);
+            this.UserNameTextBox.TabIndex = 3;
+            this.UserNameTextBox.Text = "xingkong_2000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "UserName:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(92, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Login";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 409);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using HttpHelper_namespace;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public delegate bool CallBack(int hwnd, int lParam); // 枚举窗口的回调函数
+
+    public partial class Form1 : Form
+    {
+        private List<Site> SiteList = new List<Site>();
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Navigate("https://login.taobao.com/member/login.jhtml?style=mini&newMini2=true&css_style=alimama&from=alimama&redirectURL=http%3A%2F%2Fwww.alimama.com&full_redirect=true&disableQuickLogin=true");
+        }
+
+        private string getJson1()
+        {
+            string path = @"C:\Users\sky\Documents\json1.txt";
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line = sr.ReadLine();
+            return line;
+        }
+
+        private string getJson2()
+        {
+            string path = @"C:\Users\sky\Documents\json2.txt";
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line = sr.ReadLine();
+            return line;
+        }
+
+        private void getShortLinkURL()
+        {
+            string jsonText = getJson1();
+            JObject jp1 = (JObject)JsonConvert.DeserializeObject(jsonText);
+            textBox1.Text = jp1["data"]["shortLinkUrl"].ToString();
+        }
+
+        private string utf8_unicode(string utf8String)
+        {
+            byte[] buffer1 = Encoding.Default.GetBytes(utf8String);
+            byte[] buffer2 = Encoding.Convert(Encoding.UTF8, Encoding.Default, buffer1, 0, buffer1.Length);
+            string strBuffer = Encoding.Default.GetString(buffer2, 0, buffer2.Length);
+            return strBuffer;
+        }
+
+        private void parseAdzone(String jsonText)
+        {
+            JObject jp2 = (JObject)JsonConvert.DeserializeObject(jsonText);
+            JArray adzones = (JArray)jp2["data"]["otherAdzones"];
+            foreach (var item in adzones)
+            {
+                var id = ((JObject)item)["id"];
+                var name = ((JObject)item)["name"];
+
+                Console.WriteLine("siteid : " + id + "\t" + name.ToString());
+
+                Site site = new Site();
+                site.id = id.ToString();
+                site.name = name.ToString();
+                site.zones = new List<Adzone>();
+
+                try
+                {
+                    JArray subs = (JArray)((JObject)item)["sub"];
+                    foreach (var subitem in subs)
+                    {
+                        var subid = ((JObject)subitem)["id"];
+                        var subname = ((JObject)subitem)["name"];
+                        Console.WriteLine("adzoneid : " + subid + "\t" + subname.ToString());
+                        comboBox1.Items.Add(subname.ToString());
+                        comboBox1.SelectedIndex = 0;
+
+                        Adzone adzone = new Adzone();
+                        adzone.id = subid.ToString();
+                        adzone.name = subname.ToString();
+                        site.zones.Add(adzone);
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+
+                SiteList.Add(site);
+            }
+        }
+
+        private void getAdzone()
+        {
+            string jsonText = getJson2();
+            parseAdzone(jsonText);
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool InternetGetCookieEx(string pchURL, string pchCookieName, StringBuilder pchCookieData, ref System.UInt32 pcchCookieData, int dwFlags, IntPtr lpReserved);
+        private static string GetCookies(string url)
+        {
+            uint datasize = 1024;
+            StringBuilder cookieData = new StringBuilder((int)datasize);
+            if (!InternetGetCookieEx(url, null, cookieData, ref datasize, 0x2000, IntPtr.Zero))
+            {
+                if (datasize < 0)
+                    return null;
+
+                cookieData = new StringBuilder((int)datasize);
+                if (!InternetGetCookieEx(url, null, cookieData, ref datasize, 0x00002000, IntPtr.Zero))
+                    return null;
+            }
+            return cookieData.ToString();
+        }
+
+        private void getAdzoneEx()
+        {
+            string cookie = GetCookies(webBrowser1.Document.Url.ToString());
+            HttpItem GetJuItem = new HttpItem()
+            {
+                URL = "http://pub.alimama.com/common/adzone/newSelfAdzone2.json?tag=29&t=1433864026366",
+                ContentType = "application/x-www-form-urlencoded",
+                Referer = "http://pub.alimama.com/common/adzone/newSelfAdzone2.json?tag=29&t=1433864026366",
+                Cookie = cookie,
+                Accept = "*/*",
+                UserAgent = "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)"
+            };
+            HttpHelper GetJuHelper = new HttpHelper();
+            HttpResult GetJuResult = GetJuHelper.GetHtml(GetJuItem);
+            string result = GetJuResult.Html;
+
+            parseAdzone(result);
+        }
+
+        private string getIdFromURL(string url)
+        {
+            int pos = url.IndexOf("&id=");
+            if (pos == -1)
+            {
+                pos = url.IndexOf("?id=");
+            }
+            if (pos == -1)
+            {
+                return "";
+            }
+
+            string id = url.Substring(pos + 4);
+            pos = id.IndexOf("&");
+            if (pos != -1)
+            {
+                id = id.Substring(0, pos);
+            }
+            return id;
+        }
+
+        // 获取
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getAdzoneEx();
+        }
+
+        // 转换
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 从URL中提取商品ID
+            string url = textBox1.Text;
+            string itemid = getIdFromURL(url); // 商品id
+            Console.WriteLine("id = " + itemid);
+
+            // 得到当前选择的siteid和adzoneid
+            string siteid = "", adzoneid = "";
+            string selected_adzone = comboBox1.Text;
+            foreach(Site site in SiteList)
+            {
+                bool bFind = false;
+                foreach(Adzone adzone in site.zones)
+                {
+                    if(adzone.name == selected_adzone)
+                    {
+                        Console.WriteLine("site id : " + site.id + ", adzone id : " + adzone.id);
+                        siteid = site.id;
+                        adzoneid = adzone.id;
+                        bFind = true;
+                        break;
+                    }
+                }
+                if (bFind)
+                {
+                    break;
+                }
+            }
+
+            // 转链
+            string newURL = "http://pub.alimama.com/common/code/getAuctionCode.json?";
+            newURL += "auctionid=" + itemid;
+            newURL += "&adzoneid=" + adzoneid;
+            newURL += "&siteid=" + siteid;
+            newURL += "&t=1445487172579&_input_charset=utf-8";
+
+            string cookie = GetCookies(webBrowser1.Document.Url.ToString());
+            HttpItem GetJuItem = new HttpItem()
+            {
+                URL = newURL,
+                ContentType = "application/x-www-form-urlencoded",
+                Cookie = cookie,
+                Accept = "*/*",
+                UserAgent = "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)"
+            };
+            HttpHelper GetJuHelper = new HttpHelper();
+            HttpResult GetJuResult = GetJuHelper.GetHtml(GetJuItem);
+            string result = GetJuResult.Html;
+
+            JObject jp1 = (JObject)JsonConvert.DeserializeObject(result);
+            textBox1.Text = jp1["data"]["shortLinkUrl"].ToString();
+        }
+
+        // 引用3个API
+        [DllImport("user32")]
+        public static extern int EnumWindows(CallBack x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern int GetClassName(int hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(int hWnd, StringBuilder lpString, int nMaxCount);
+        private bool EnumWindowsProc(int hwnd, int param)
+        {
+            StringBuilder className = new StringBuilder(200);
+            int len;
+            len = GetClassName(hwnd, className, 200);
+            if(len > 0)
+            {
+                if(className.ToString() == "TXGuiFoundation")
+                {
+                    StringBuilder wndName = new StringBuilder(200);
+                    int nameLen;
+                    nameLen = GetWindowText(hwnd, wndName, 200);
+                    if (nameLen > 0)
+                    {
+                        string strWndName = wndName.ToString();
+                        if (strWndName != "QQ" && strWndName != "TXMenuWindow")
+                        {
+                            listBox1.Items.Add(wndName.ToString());
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        // 获取QQ
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            CallBack myCallBack = new CallBack(EnumWindowsProc);
+            EnumWindows(myCallBack, 0);
+        }
+
+        // 群发
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 登录
+        private void button5_Click(object sender, EventArgs e)
+        {
+            HtmlElement nameText = webBrowser1.Document.GetElementById("TPL_username_1");
+            HtmlElement passwordText = webBrowser1.Document.GetElementById("TPL_password_1");
+            nameText.InnerText = UserNameTextBox.Text;
+            passwordText.InnerText = PasswordTextBox.Text;
+
+            HtmlElement submitButton = webBrowser1.Document.GetElementById("J_SubmitStatic");
+            submitButton.InvokeMember("click");
+        }
+    }
+
+    class Site
+    {
+        public string id;
+        public string name;
+        public List<Adzone> zones;
+    }
+
+    class Adzone
+    {
+        public string id;
+        public string name;
+    }
+}
